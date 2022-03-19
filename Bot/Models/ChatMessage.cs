138d6a2aa18fed0d7345bf2421c5ed22e4fb3437
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-19 12:40:19 -0600

    View cells and template selectors

---
+++ Bot/Models/ChatMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Bot.Models
+{
+    public class ChatMessage
+    {
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        public bool IsIncoming
+        {
+            get;
+            set;
+        }
+    }
+}
