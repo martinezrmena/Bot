b2c4fead43e76acec4970622bf2346cd9e4eb5f0
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-19 11:10:57 -0600

    Calling the Bot API

---
+++ Bot/ViewModel/Helpers/BotServiceHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Bot.ViewModel.Helpers
+{
+    public class BotServiceHelper
+    {
+        public Conversation _Conversation
+        {
+            get;
+            set;
+        }
+
+        public BotServiceHelper()
+        {
+            CreateConversation();
+        }
+
+        private async void CreateConversation()
+        {
+            string endpoint = "/v3/directline/conversations";
+
+            using(HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://directline.botframework.com");
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer BoKJEGd1SlE.aSssnudGFJhgBuZ7wXqyUdd8eNoDXjF2ISsm15HUlsw");
+
+                var response = await client.PostAsync(endpoint, null);
+                string json = await response.Content.ReadAsStringAsync();
+
+                _Conversation = JsonConvert.DeserializeObject<Conversation>(json);
+            }
+        }
+
+        public class Conversation
+        {
+            public string ConversationId
+            {
+                get;
+                set;
+            }
+
+            public string Token
+            {
+                get;
+                set;
+            }
+
+            public string StreamUrl
+            {
+                get;
+                set;
+            }
+
+            public string ReferenceGrammarId
+            {
+                get;
+                set;
+            }
+
+            public int Expires_in
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
---
+++ Bot/ViewModel/MainVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Bot.ViewModel.Helpers;
+
+namespace Bot.ViewModel
+{
+    public class MainVM
+    {
+        BotServiceHelper botHelper;
+        public MainVM()
+        {
+            botHelper = new BotServiceHelper();
+        }
+    }
+}
