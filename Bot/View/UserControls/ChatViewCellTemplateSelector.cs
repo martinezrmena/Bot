e311bac5d9126d7a559cd79cdbd468ef850ecd45
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-19 12:40:56 -0600

    View cells and template selectors

---
+++ Bot/View/UserControls/ChatViewCellTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using static Bot.ViewModel.MainVM;
+
+namespace Bot.View.UserControls
+{
+    public class ChatViewCellTemplateSelector : DataTemplateSelector
+    {
+        private readonly DataTemplate incomingDataTemplate;
+        private readonly DataTemplate outgoingDataTemplate;
+
+        public ChatViewCellTemplateSelector()
+        {
+            incomingDataTemplate = new DataTemplate(typeof(IncomingViewCell));
+            outgoingDataTemplate = new DataTemplate(typeof(OutgoingViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var chatMessage = item as ChatMessage;
+            if(chatMessage != null)
+            {
+                return chatMessage.IsIncoming ? incomingDataTemplate : outgoingDataTemplate;
+            }
+            return null;
+        }
+    }
+}
---
+++ Bot/View/UserControls/OutgoingViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Bot.View.UserControls
+{
+    public partial class OutgoingViewCell : ViewCell
+    {
+        public OutgoingViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
